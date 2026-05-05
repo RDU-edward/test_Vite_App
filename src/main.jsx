e8a5fbddab1215e0d24bf430aa036e21fa28900d
RDU-edward <catapan.edward@wealthbank.com.ph>
2026-05-05 17:36:21 +0800

    v.5.5.20261

--- src/main.jsx
+++ src/main.jsx
@@ -32,3 +32,76 @@ createRoot(document.getElementById("root")).render(
 // VITE_API_URL = "http://localhost:3000/api/"
 // VITE_GOOGLE_MAPS_SECRET_KEY = "AIzaSyAWCKewC7vdKWUSiZq85---sDBK0LVAWRo"
 // VITE_STRIPE_SECRET_KEY = "pk_test_8d1dup9d4LZOCQ8kuU5HS7Wm00XAXX9zO9"
+
+// # DB_HOST=loacalhost
+// # DB_USER=root
+// # DB_PASS=root
+// # DB_NAME=test
+
+// # Node Mailer Credentials
+// # EMAIL_USER = "edwardcatapan@gmail.com"
+// # EMAIL_PASS = "scah xnqy fhcc retl",
+// STRIPE_SECRET_KEY = sk_test_eqXnJVeXFUmkfE7b3FZkogGo00bG3wOC02;
+
+// // utils/mailer.js
+// require("dotenv").config();
+// const nodemailer = require("nodemailer");
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// async function sendEmail({ to, subject, text, html }) {
+//   try {
+//     const info = await transporter.sendMail({
+//       from: "<noreply@gmail.com>",
+//       to,
+//       subject,
+//       text,
+//       html,
+//     });
+
+//     console.log("Email sent:", info.messageId);
+//   } catch (err) {
+//     console.error("Error sending email:", err);
+//   }
+// }
+
+// module.exports = sendEmail;
+
+// async function test() {
+//   await sendEmail({
+//     to: "jezmacoy1998@gmail.com",
+//     subject: "New Manager Account Pending Approval",
+//     text: `Hello Admin,
+
+// A new Manager account has been registered and is pending your approval.
+
+// Please review and approve the account at: https://www.ihomes.com/admin
+
+// Thank you for your attention.
+
+// Best regards,
+// iHomes Team`,
+//     html: `<h2>New Manager Account Pending For Approval</h2>
+// <p>Hello Admin,</p>
+
+// <p>A new Manager account has been registered and is <strong>pending for your approval</strong>.</p>
+
+// <p>Please review and approve the account by clicking the link below:</p>
+
+// <p><a href="https://www.ihomes.com/admin">Approve Account</a></p>
+
+// <p>Thank you for your attention.</p>
+
+// <p>Best regards,<br>
+// <i>iHomes Team</i></p>`,
+//   });
+// }
+
+// test();
